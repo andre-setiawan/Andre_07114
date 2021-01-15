0ff35f3692c07367edc7be35e30517cb65a7849a
Andre_9 <72396078+andre-setiawan@users.noreply.github.com>
2021-01-15 15:01:04 +0700

    project modul4 sebelum UAS

---
+++ Praktikum/Modul_4/SISKO_AKHIR/src/Entity/andre07114_AdminEntity.java
@@ -0,0 +1,27 @@
+package Entity;
+
+public class andre07114_AdminEntity {
+    private String nama, code;
+
+    public andre07114_AdminEntity(String nama, String code) {
+        this.nama   = nama;
+        this.code = code;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
+
---
+++ Praktikum/Modul_4/SISKO_AKHIR/src/Entity/andre07114_KelasEntity.java
@@ -0,0 +1,26 @@
+package Entity;
+
+public class andre07114_KelasEntity {
+    public String nama, code;
+
+    public andre07114_KelasEntity(String nama, String code) {
+        this.nama   = nama;
+        this.code = code;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
