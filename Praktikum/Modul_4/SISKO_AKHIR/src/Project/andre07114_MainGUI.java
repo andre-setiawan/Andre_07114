6261cc682b3ac05273a58352161a33cc1c67ca11
Andre_9 <72396078+andre-setiawan@users.noreply.github.com>
2021-01-19 00:26:53 +0700

    Delete Praktikum/Modul_4 directory

--- Praktikum/Modul_4/SISKO_AKHIR/src/Project/andre07114_MainGUI.java
+++
@@ -1,9 +0,0 @@
-package Project;
-
-import GUI.andre07114_GUI;
-
-public class andre07114_MainGUI {
-    public static void main(String[] args) {
-        andre07114_GUI gui = new andre07114_GUI();
-    }
-}
